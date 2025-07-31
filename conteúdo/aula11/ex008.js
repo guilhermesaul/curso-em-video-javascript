2b82c208520eeb65536acac8d76305612f0d6c65
guisaul <guilhermesaul190@gmail.com>
2025-07-31 08:48:30 -0300

    Aula 11 finalizada (condições)

---
+++ conteúdo/aula11/ex008.js
@@ -0,0 +1,9 @@
+let vel;
+vel = 172;
+
+// condição simples
+if (vel > 60) {
+  console.log("Você está mutado");
+}
+
+console.log("Velocidade permitida");
---
+++ conteúdo/aula11/ex009.js
@@ -0,0 +1,9 @@
+let pais;
+pais = "EUA";
+
+// condição composta
+if (pais === "Brasil") {
+  console.log("Você é Brasileiro");
+} else {
+  console.log("Você é Estrangeiro");
+}
